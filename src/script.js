2b1a7c133a188f653f8cf431687e0fff8fb775f7
anchuyu <anchuyu@outlook.jp>
2026-09-05 20:37:50 +0900

    feat: add date info & add script & css

---
+++ src/script.js
@@ -0,0 +1,33 @@
+function count(game) {
+    fetch("/click", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ game }),
+    }).catch((err) => {
+        console.error("Failed to send click:", err);
+    });
+}
+
+async function getAnalysis() {
+    try {
+        const res = await fetch("/popular");
+        if (!res.ok) throw new Error("Server error");
+        return await res.json(); // ← JSON に変換
+    } catch (err) {
+        console.error("Failed to get analysis:", err);
+        return null;
+    }
+}
+
+async function getGameData(path = "/src/data.json") {
+    try {
+        const res = await fetch(path);
+        if (!res.ok) throw new Error("Server error");
+        return await res.json();
+    } catch (err) {
+        console.error("Failed to get resources:", err);
+        return null;
+    }
+}
